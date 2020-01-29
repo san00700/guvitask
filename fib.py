e84584806888c24d3a8d2f9c5ba8f7342d07fbe3
san00700 <60385327+san00700@users.noreply.github.com>
2020-01-29 12:12:35 +0530

    Update fib.py

--- fib.py
+++ fib.py
@@ -4,15 +4,15 @@ i = 0
 First = 0
 Second = 1 
 while(i < n):
-  if(i <= 1):
-     temp = i
-  else:
-     temp = First + Second
-     First = Second
-     Second = temp
-print( temp )
-a.append(temp)
+    if(i <= 1):
+       temp = i
+    else:
+       temp = First + Second
+       First = Second
+       Second = temp
+    print( temp )
+    a.append(temp)
+    i = i + 1
 print(a)
-i = i + 1
   
   
